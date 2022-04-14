ff25594a306d194e80164ed904c7dc2ee3b0925e
Parbaz-Hossain <md.parvezcse@gamil.com>
2022-04-14 15:43:35 +0600

    April 14 2022

--- ViewModel/EmployeedetailVM.cs
+++ ViewModel/EmployeedetailVM.cs
@@ -1,20 +1,23 @@
-﻿using System;
+﻿using CORE05.Models;
+using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-#nullable disable
-
-namespace CORE05.Models
+namespace CORE05.ViewModel
 {
-    public partial class Employeedetail
+    public class EmployeedetailVM
     {
         public int Id { get; set; }
         [StringLength(12, MinimumLength = 8)]
-        [Required(ErrorMessage ="Please Enter 12 didgit Id")]
+        [Required(ErrorMessage = "Please Enter 12 didgit Id")]
         public string Employeeid { get; set; }
         [StringLength(255)]
-        [Display(Name ="Employee Name")]
+        [Display(Name = "Employee Name")]
         [Required(ErrorMessage = "Please Enter Employee Name")]
         public string Employeename { get; set; }
         public DateTime Joiningdate { get; set; }
@@ -27,12 +30,13 @@ namespace CORE05.Models
         [NotMapped]
         public string hobbiesName { get; set; }
         [NotMapped]
-        [Display(Name ="Hobbies")]
+        [Display(Name = "Hobbies")]
         public string[] hobbiesArr { get; set; }
 
         public string Address { get; set; }
+        public IFormFile ProfileImage { get; set; }
 
-        [ForeignKey("Districtid")]       
+        [ForeignKey("Districtid")]
         public virtual Districtinf District { get; set; }
     }
 }
