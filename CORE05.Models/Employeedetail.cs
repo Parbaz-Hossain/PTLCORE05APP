f1e92dca3979d3c5d26fa48a3f09624c5a39e701
Parbaz_Hossain <md.parvezcse@gmail.com>
2022-04-17 21:56:01 +0530

    Changes

--- CORE05.Models/Employeedetail.cs
+++ CORE05.Models/Employeedetail.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 #nullable disable
 
@@ -16,13 +17,14 @@ namespace CORE05.Models
         public string Employeename { get; set; }
         public DateTime Joiningdate { get; set; }
         public string Gender { get; set; }
-        public int Districtid { get; set; }
         public bool Active { get; set; }
         public string Image { get; set; }
         public decimal Salary { get; set; }
         public string Hobbiesid { get; set; }
         public string Address { get; set; }
 
+        public int Districtid { get; set; }
+        [ForeignKey("Districtid")]
         public virtual Districtinf District { get; set; }
     }
 }
